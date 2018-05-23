8eed0749a075ae8f6aaa69afc3deb5b93600d359
Rom Levtov <romgamesrom@gmail.com>
2018-05-23 22:04:34 +0300

    added some more dankess to the about page.

--- pages/about.php
+++ pages/about.php
@@ -31,12 +31,32 @@
         left: 240;
         right: 0;
     }
+
+    #zerobyterProfilePic{
+        width: 180px;
+        border-width: 2px;
+        border-style: solid;
+        border-color: #00000026;
+        border-radius: 180px;
+        transition: border-color 500ms;
+    }
+
+    #zerobyterProfilePic:hover{
+        border-color: blue;
+    }
 </style>
 
 <center>
     <h2>ZeroPanel is developed by ZeroByter, it is open source on <a href="https://github.com/ZeroByter/zeropanelv2" target="_blank">GitHub</a>.</h2>
     <h3>ZeroPanel is entirely based off <a href="https://github.com/Cyberbyte-Studios/CyberWorks" target="_blank">CyberWorks</a>' panel, ZeroPanel is simply redesigned and less buggy with modern Arma than <a href="https://github.com/Cyberbyte-Studios/CyberWorks" target="_blank">CyberWorks</a>.</h3>
-    
+    <br><br><br><br><br>
+    <h2 style="width: 60%;">Like using ZeroPanelV2? Please considering helping <a href="http://www.zerobyter.net" target="_blank">me</a> out by donating to my PayPal! My EMail is <a href="javascript:void(0)">zerobyter@zerobyter.net</a>. Donate how much you want! Any help is appreciated.</h2>
+    <div id="zerobyterProfilePic" style="width:180px;">
+        <a href="http://www.zerobyter.net" target="_blank">
+            <img src="/css/most_handsome_man.png" style="width:180px;"></img>
+        </a>
+    </div>
+
     <div id="versionText">
         <?php
             $compareVersions = compareVersions();
